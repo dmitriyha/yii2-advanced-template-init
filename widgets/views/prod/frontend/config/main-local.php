69f292f925c5c296213b59a8e1f39e431fe93cac
Dima <diharalson@gmail.com>
2016-09-21 22:34:37 +0300

    fix validatin creation

--- widgets/views/prod/frontend/config/main-local.php
+++ widgets/views/prod/frontend/config/main-local.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '"md5(uniqid())"',
+            'cookieValidationKey' => '".md5(uniqid())."',
         ],
     ],
 ];";
