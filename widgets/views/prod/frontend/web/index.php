483b62a82e45193868088a6ab1671158d0046ab8
Dima <diharalson@gmail.com>
2016-09-21 22:22:00 +0300

    prod implemented

--- widgets/views/prod/frontend/web/index.php
+++ widgets/views/prod/frontend/web/index.php
@@ -1,4 +1,5 @@
 <?php
+echo "<?php
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'prod');
 
@@ -7,12 +8,12 @@ require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
 
-$config = yii\helpers\ArrayHelper::merge(
+\$config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../common/config/main.php'),
     require(__DIR__ . '/../../common/config/main-local.php'),
     require(__DIR__ . '/../config/main.php'),
     require(__DIR__ . '/../config/main-local.php')
 );
 
-$application = new yii\web\Application($config);
-$application->run();
+\$application = new yii\web\Application(\$config);
+\$application->run();";
